a907588a02d5fd091c9cec4f267309ce755a0edd
Shehan-Anuradha <88441609+Shehan-Anuradha@users.noreply.github.com>
2022-08-27 14:08:42 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A person wearing a uniform and a badge, indicating they’re with a printer repair company, is sitting unsupervised at a coworker’s desk. They seem to be looking through the files and documents that are visible on that machine. It seems suspicious, so you ask if they need some assistance. They tell you that the printer is having problems, so they were looking for a file they could print, to diagnose the problem better. Which of the following is the biggest red flag in this scenario?",
+    answer: "They are looking through the files on the system.",
+    options: [
+      "They are trying to diagnose a problem with a printer.",
+      "There is a repair person wearing a uniform.",
+      "They are looking through the files on the system.",
+      "They are sitting unescorted at a coworker's desk."
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+];
