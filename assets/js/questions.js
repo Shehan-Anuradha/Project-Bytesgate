feae3e02775468aa292eb8308260a6d3ab7eabd3
Shehan-Anuradha <88441609+Shehan-Anuradha@users.noreply.github.com>
2022-10-23 05:59:27 +0530

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A person wearing a uniform and a badge, indicating they’re with a printer repair company, is sitting unsupervised at a coworker’s desk. They seem to be looking through the files and documents that are visible on that machine. It seems suspicious, so you ask if they need some assistance. They tell you that the printer is having problems, so they were looking for a file they could print, to diagnose the problem better. Which of the following is the biggest red flag in this scenario?",
+    answer: "They are looking through the files on the system.",
+    options: [
+      "They are trying to diagnose a problem with a printer.",
+      "There is a repair person wearing a uniform.",
+      "They are looking through the files on the system.",
+      "They are sitting unescorted at a coworker's desk."
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following statements is true",
+    answer: "Emails that request sensitive information of any sort should be treated with skepticism.",
+    options: [
+      "Emails that request sensitive information of any sort should be treated with skepticism.",
+      "Emails from your boss and other co-workers can always be trusted.",
+      "Emails with spammy content are harmless.",
+      "Emails containing attachments are usually malicious."
+    ]
+  },
+    {
+    numb: 3,
+    question: "You receive a generic email notification about an issue with the delivery of a recent order. You’re instructed to click on a link included in the email for further information about the order. When you click, it opens a webpage which asks for your account username and password before you can view the information. Which of the following is the biggest red flag in this scenario?",
+    answer: "Your username and password are required to view the information.",
+    options: [
+      "There is an issue while delivering your order.",
+      "The email you received was a generic notification.",
+      "The information about the delivery problems is not contained in the email.",
+      "Your username and password are required to view the information."
+    ]
+  },
+    {
+    numb: 4,
+    question: "Someone from the IT department calls you and states that he needs your username and password so the can run an update on your computer. How should you handle this situation?",
+    answer: "Promptly end the phone call and report the incident to management.",
+    options: [
+      "Promptly end the phone call and report the incident to management.",
+      "Tell him you'll email your username and password to the IT department because that's a safer form of communication.",
+      "Give him your username and password since updates are a crucial part of security.",
+      "Put him on hold and ask your co-worker if their computer has been updated."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of these is how to properly avoid becoming a victim of social engineering?",
+    answer: "All of the above",
+    options: [
+      "Research the facts",
+      "Delete any request for financial information or passwords",
+      "Reject requests for help",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of the following are examples of social engineering scams executed via phishing?",
+    answer: "All of the above",
+    options: [
+      "Banking Link Scams",
+      "Fax Notice Scams",
+      "Facebook Message Link Scams",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "An email claiming that you have won the lottery, as long as you fill out the corresponding information, is an example of what type social engineering attack?",
+    answer: "Phishing",
+    options: [
+      "Baiting",
+      "Phishing",
+      "Piggybacking",
+      "Email from a friend"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Spammers want you to:",
+    answer: "Act first and think later",
+    options: [
+      "Not open any links",
+      "Think first and act later",
+      "Act first and think later",
+      "Update your software automatically"
+    ]
+  },
+  {
+    numb: 9,
+    question: "When attempting to obtain someone's information, social engineering tactics are usually utilized less than simply hacking someone's software.",
+    answer: "False",
+    options: [
+      "True",
+      "False",
+      "May be",
+      "I don't know"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the goal of social engineering?",
+    answer: "To gain vital personal information",
+    options: [
+      "Sabotage a person's social media",
+      "To catfish someone",
+      "To gain vital personal information",
+      "To build trust"
+    ]
+  },
+
+];
